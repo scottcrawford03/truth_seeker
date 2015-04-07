226e93555d49786667785cc4fb97092271e94a94
Laura Whalin <laurawhalin@gmail.com>
2015-04-07 07:53:35 -0600

    fixing up some hound issues

--- app/models/create_instagram_post.rb
+++ app/models/create_instagram_post.rb
@@ -1,7 +1,7 @@
 class CreateInstagramPost
   attr_reader :service, :posts
 
-  def initialize(service=InstagramService.new)
+  def initialize(service = InstagramService.new)
     @service = service
   end
 
@@ -11,21 +11,23 @@ class CreateInstagramPost
 
   def find_and_save_all_posts
     Tag.all.flat_map do |tag|
-      service.find_posts_in_batches(tag.hashtag, &method(:save_posts_to_database))
+      service.find_posts_in_batches(tag.hashtag,
+                                    &method(:save_posts_to_database)
+                                    )
     end
   end
 
   def save_posts_to_database(posts, tag)
     posts.each do |post|
-      location = post['location'] || {}
-      caption = post['caption'] || {'created_time' => 0}
-      image = post['images'] || {'standard_resolution' => {}}
-      Post.where(uuid: post['id'], provider: 'instagram').first_or_create(
-        lat: location['latitude'],
-        long: location['longitude'],
-        posted_at: Time.at(caption['created_time'].to_i),
-        text: caption['text'],
-        image: image['standard_resolution']['url'],
+      location = post["location"] || {}
+      caption = post["caption"] || { "created_time" => 0 }
+      image = post["images"] || { "standard_resolution" => {} }
+      Post.where(uuid: post["id"], provider: "instagram").first_or_create(
+        lat: location["latitude"],
+        long: location["longitude"],
+        posted_at: Time.at(caption["created_time"].to_i),
+        text: caption["text"],
+        image: image["standard_resolution"]["url"],
         category_id: Tag.find_by(hashtag: tag).category_id,
       )
     end
