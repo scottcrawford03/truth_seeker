398f481df0a43cb3fb0888f2ec5041d4b60f237f
KristaANelson <krista.a.nelson@gmail.com>
2015-04-07 15:04:28 -0600

    update hash format and remove puts for hound

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,7 @@
 class Seed
   def initialize
-    p "=============================="
-    p "Seeding started"
-    p "=============================="
     generate_categories
     generate_tags
-    p "=============================="
-    p "Seed data loaded"
-    p "=============================="
   end
 
   def category_names
@@ -15,42 +9,41 @@ class Seed
   end
 
   def tag_data
-    { 1=>["elvissighting",
-          "elvisisalive",
-          "elvislives",
-          "elvispresleylives",
-          "elvisalive",
-          "foundelvis"],
-      2=>["bigfootsighting",
-          "bigfootisreal",
-          "bigfootfound",
-          "foundbigfoot",
-          "squatchsighting",
-          "squatchin"],
-      3=>["ufosighting",
-          "realufo",
-          "ufofound",
-          "ufo",
-          "ufos",
-          "flyingsaucer"],
-      4=>["ghost",
-          "ghostsighting",
-          "ghosthunter",
-          "hauntedhouse",
-          "haunted",
-          "hauntedghost",
-          "paranormalactivity",
-          "aliensighting"],
-      5=>["tupaclives",
-          "tupacisalive",
-          "foundtupac",
-          "tupacalive"] }
+    { 1 => ["elvissighting",
+             "elvisisalive",
+             "elvislives",
+             "elvispresleylives",
+             "elvisalive",
+             "foundelvis"],
+      2 => ["bigfootsighting",
+            "bigfootisreal",
+            "bigfootfound",
+            "foundbigfoot",
+            "squatchsighting",
+            "squatchin"],
+      3 => ["ufosighting",
+            "realufo",
+            "ufofound",
+            "ufo",
+            "ufos",
+            "flyingsaucer"],
+      4 => ["ghost",
+            "ghostsighting",
+            "ghosthunter",
+            "hauntedhouse",
+            "haunted",
+            "hauntedghost",
+            "paranormalactivity",
+            "aliensighting"],
+      5 => ["tupaclives",
+            "tupacisalive",
+            "foundtupac",
+            "tupacalive"] }
   end
 
   def generate_categories
     category_names.each do |name|
       category = Category.create(name: name)
-      puts "Category #{category.name} was created!"
     end
   end
 
@@ -58,7 +51,6 @@ class Seed
     tag_data.each do |category_id, tags|
       tags.each do |tag|
         Tag.create(hashtag: tag, category_id: category_id)
-        puts "Tag #{tag} create, with category_id: #{category_id}"
       end
     end
   end
